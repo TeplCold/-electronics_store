4905cdbd5a2f017ad445734aafecff69baf6c8e3
Георгий <teplcold@gmail.com>
2020-06-20 20:18:17 +0400

    добавил стили панель управления

--- admin_control_panel/clients.php
+++ admin_control_panel/clients.php
@@ -43,148 +43,157 @@ if ($_SESSION['auth_login'] == 'admin') { //выводим эту страниц
 
         <?php include("header.php"); ?>
 
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="admin_control_panel.php">Главная</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Клиенты/администраторы</li>
-            </ol>
-        </nav>
+        <div class="container block_cuntent">
 
-        <?php include("panelyprav.php"); ?>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="admin_control_panel.php">Главная</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Клиенты/администраторы</li>
+                </ol>
+            </nav>
 
-
-
-
-        <?php
-        $all_client = mysqli_query($link, "SELECT * FROM users WHERE login != 'admin' ");
-        $result_count = mysqli_num_rows($all_client);
-
-        $all_admin = mysqli_query($link, "SELECT * FROM users WHERE login = 'admin'");
-        $admin_count = mysqli_num_rows($all_admin);
-        ?>
+            <?php include("panelyprav.php"); ?>
 
 
-        <p id="count-clients">Клиенты - <strong><?php echo $result_count; ?></strong></p>
-        <p id="count-admin">Администраторы - <strong><?php echo  $admin_count; ?></strong></p>
 
 
-        <div class="container-fluid">
             <?php
+            $all_client = mysqli_query($link, "SELECT * FROM users WHERE login != 'admin' ");
+            $result_count = mysqli_num_rows($all_client);
 
-            $num = 2;
-            $page = (int) $_GET['page'];
+            $all_admin = mysqli_query($link, "SELECT * FROM users WHERE login = 'admin'");
+            $admin_count = mysqli_num_rows($all_admin);
+            ?>
 
-            $count = mysqli_query($link, "SELECT COUNT(*) FROM users ");
-            $temp = mysqli_fetch_array($count);
 
-            if ($temp[0] > 0) {
-                $tempcount = $temp[0];
+            <p id="count-clients">Клиенты - <strong><?php echo $result_count; ?></strong></p>
+            <p id="count-admin">Администраторы - <strong><?php echo  $admin_count; ?></strong></p>
 
-                // находим общее число страниц 
-                $total = (($tempcount - 1) / $num) + 1;
-                $total =  intval($total);
-                $page = intval($page);
 
-                if (empty($page) or $page < 0) {
-                    $page = 1;
+            <div class="container-fluid">
+                <?php
+
+                $num = 10;
+                $page = (int) $_GET['page'];
+
+                $count = mysqli_query($link, "SELECT COUNT(*) FROM users ");
+                $temp = mysqli_fetch_array($count);
+
+                if ($temp[0] > 0) {
+                    $tempcount = $temp[0];
+
+                    // находим общее число страниц 
+                    $total = (($tempcount - 1) / $num) + 1;
+                    $total =  intval($total);
+                    $page = intval($page);
+
+                    if (empty($page) or $page < 0) {
+                        $page = 1;
+                    }
+                    if ($page > $total) {
+                        $page = $total;
+                    }
+                    // вычисляем с какого номера начинать следует выводить товар
+                    $start = $page * $num - $num;
+                    $qury_start_num = " LIMIT $start, $num";
                 }
-                if ($page > $total) {
-                    $page = $total;
-                }
-                // вычисляем с какого номера начинать следует выводить товар
-                $start = $page * $num - $num;
-                $qury_start_num = " LIMIT $start, $num";
-            }
 
 
-            $result = mysqli_query($link, "SELECT * FROM users    ORDER BY id DESC LIMIT $start, $num");
-            if (mysqli_num_rows($result) > 0) {
-                $row = mysqli_fetch_array($result);
-                do {
-                    $datetime = strtotime($row["datetime"]);
-                    echo '
+                $result = mysqli_query($link, "SELECT * FROM users    ORDER BY id DESC LIMIT $start, $num");
+                if (mysqli_num_rows($result) > 0) {
+                    $row = mysqli_fetch_array($result);
+                    do {
+                        $datetime = strtotime($row["datetime"]);
+                        echo '
                     <div class="block-clients">
-                    -----------------------------------------------------------------------
+               
+                    <div class="block-clientsflex">
                     <p class="client-login" ><strong>' . $row["login"] . '</strong></p>
+                    <div class="client-links" ><a class="delete3" rel="clients.php?id=' . $row["id"] . '&action=delete" >Удалить</a></div>
+                    </div> 
+                    
+                        <ul>
+                            <li><strong>E-Mail</strong> - ' . $row["email"] . '</>
+                            <li><strong>Фамилия</strong> - ' . $row["surname"] . '</li>
+                            <li><strong>Имя</strong> -  ' . $row["name"] . '</li>
+                            <li><strong>Отчество</strong> - ' . $row["patronymic"] . '</li>
+                            <li><strong>IP</strong> - ' . $row["ip"] . '</li>
+                            <li><strong>Дата регистрации</strong> - ' .  date("d.m.Y h:i:s", $datetime) . '</li>
+                        </ul>
                    
-                  
-                    <ul>
-                        <li><strong>E-Mail</strong> - ' . $row["email"] . '</>
-                        <li><strong>Фамилия</strong> - ' . $row["surname"] . '</li>
-                        <li><strong>Имя</strong> -  ' . $row["name"] . '</li>
-                        <li><strong>Отчество</strong> - ' . $row["patronymic"] . '</li>
-                        <li><strong>IP</strong> - ' . $row["ip"] . '</li>
-                        <li><strong>Дата регистрации</strong> - ' .  date("d.m.Y h:i:s", $datetime) . '</li>
-                    </ul>
-                    <div><p class="client-links" ><a class="delete3" rel="clients.php?id=' . $row["id"] . '&action=delete" >Удалить</a></p></div> 
                     </div>
                     ';
-                } while ($row = mysqli_fetch_array($result));
-            }
+                    } while ($row = mysqli_fetch_array($result));
+                }
 
 
-            ?>
-        </div>
+                ?>
+            </div>
 
-        <div class="container-fluid" id="pagination">
-            <?php
-            if ($page != 1) {
-                $pstr_prev = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 1) . '">    <   </a></li>';
-            }
-            if ($page != $total) {
-                $pstr_next = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 1) . '">    >   </a></li>';
-            }
+            <div class="container-fluid" id="pagination">
+                <?php
+                if ($page != 1) {
+                    $pstr_prev = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 1) . '">    <   </a></li>';
+                }
+                if ($page != $total) {
+                    $pstr_next = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 1) . '">    >   </a></li>';
+                }
 
-            // Формируем ссылки со страницами
+                // Формируем ссылки со страницами
 
-            if ($page - 4 > 0) $page4left = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 4) . '">' . ($page - 4) . '</a></li>';
-            if ($page - 3 > 0) $page3left = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 3) . '">' . ($page - 3) . '</a></li>';
-            if ($page - 2 > 0) $page2left = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 2) . '">' . ($page - 2) . '</a></li>';
-            if ($page - 1 > 0) $page1left = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 1) . '">' . ($page - 1) . '</a></li>';
-            if ($page + 4 <= $total) $page4right = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 4) . '">' . ($page + 4) . '</a></li>';
-            if ($page + 3 <= $total) $page3right = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 3) . '">' . ($page + 3) . '</a></li>';
-            if ($page + 2 <= $total) $page2right = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 2) . '">' . ($page + 2) . '</a></li>';
-            if ($page + 1 <= $total) $page1right = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 1) . '">' . ($page + 1) . '</a></li>';
+                if ($page - 4 > 0) $page4left = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 4) . '">' . ($page - 4) . '</a></li>';
+                if ($page - 3 > 0) $page3left = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 3) . '">' . ($page - 3) . '</a></li>';
+                if ($page - 2 > 0) $page2left = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 2) . '">' . ($page - 2) . '</a></li>';
+                if ($page - 1 > 0) $page1left = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page - 1) . '">' . ($page - 1) . '</a></li>';
+                if ($page + 4 <= $total) $page4right = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 4) . '">' . ($page + 4) . '</a></li>';
+                if ($page + 3 <= $total) $page3right = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 3) . '">' . ($page + 3) . '</a></li>';
+                if ($page + 2 <= $total) $page2right = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 2) . '">' . ($page + 2) . '</a></li>';
+                if ($page + 1 <= $total) $page1right = '<li class="page-item"><a class="page-link" href="clients.php?page=' . ($page + 1) . '">' . ($page + 1) . '</a></li>';
 
-            if ($page + 5 == $total) {
-                $strtotal1 = '<li><a class="page-link" href="clients.php?page=' . $total . '">' . $total . '</a></li>';
-            } elseif ($page + 4 < $total) {
-                $strtotal1 = '<li class="page-item"><a class="page-link">...</a></li><li><a class="page-link" href="clients.php?page=' . $total . '">' . $total . '</a></li>';
-            } else {
-                $strtotal1 = "";
-            }
+                if ($page + 5 == $total) {
+                    $strtotal1 = '<li><a class="page-link" href="clients.php?page=' . $total . '">' . $total . '</a></li>';
+                } elseif ($page + 4 < $total) {
+                    $strtotal1 = '<li class="page-item"><a class="page-link">...</a></li><li><a class="page-link" href="clients.php?page=' . $total . '">' . $total . '</a></li>';
+                } else {
+                    $strtotal1 = "";
+                }
 
-            if ($page == 6) {
-                $strtotal2 = '<li><a class="page-link" href="clients.php?page=' . 1 . '">' . 1 . '</a></li>';
-            } elseif ($page - 4 > 1) {
-                $strtotal2 = '<li><a class="page-link" href="clients.php?page=' . 1 . '">' . 1 . '</a></li> <li class="page-item"><a class="page-link">...</a></li>';
-            } else {
-                $strtotal2 = "";
-            }
+                if ($page == 6) {
+                    $strtotal2 = '<li><a class="page-link" href="clients.php?page=' . 1 . '">' . 1 . '</a></li>';
+                } elseif ($page - 4 > 1) {
+                    $strtotal2 = '<li><a class="page-link" href="clients.php?page=' . 1 . '">' . 1 . '</a></li> <li class="page-item"><a class="page-link">...</a></li>';
+                } else {
+                    $strtotal2 = "";
+                }
 
-            if ($total > 1) {
-                echo '
+                if ($total > 1) {
+                    echo '
             <nav class="container_pagination" aria-label="Page navigation example">
             <ul class="pagination">
             ';
-                echo $pstr_prev . $strtotal2  . $page4left . $page3left . $page2left . $page1left .
-                    "<li class='page-item active' ><p class='page-link' href='tovar.php?sort=" . $_GET["sort"] . " &page=" . $page . "'>" . $page . "</p></li>" . $page1right . $page2right . $page3right . $page4right  . $strtotal1 . $pstr_next;
-                echo '
+                    echo $pstr_prev . $strtotal2  . $page4left . $page3left . $page2left . $page1left .
+                        "<li class='page-item active' ><p class='page-link' href='tovar.php?sort=" . $_GET["sort"] . " &page=" . $page . "'>" . $page . "</p></li>" . $page1right . $page2right . $page3right . $page4right  . $strtotal1 . $pstr_next;
+                    echo '
             </ul>
             </nav>
             ';
-            }
-            ?>
+                }
+                ?>
+            </div>
+
+
+            <script defer type="text/javascript" src="../javascript/jquery-3.5.1.js"> </script>
+            <script defer type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+
+            <script defer type="text/javascript" src="../javascript/jquery-3.4.1.js"></script>
+            <script defer type="text/javascript" src="js/script.js"></script>
+            <script defer type="text/javascript" src="jquery_confirm/jquery_confirm.js"></script>
+            <script defer type="text/javascript" src="../javascript/header_footer.js"></script>
+
+            <script defer type="text/javascript" src="../javascript/scrollup.js"></script>
         </div>
-
-
-        <script defer type="text/javascript" src="../javascript/jquery-3.5.1.js"> </script>
-        <script defer type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
-
-        <script defer type="text/javascript" src="../javascript/jquery-3.4.1.js"></script>
-        <script defer type="text/javascript" src="js/script.js"></script>
-        <script defer type="text/javascript" src="jquery_confirm/jquery_confirm.js"></script>
-        <script defer type="text/javascript" src="../javascript/header_footer.js"></script>
+        <?php include("footer.php") ?>
+        <a href="#" class="scrollup">Наверх</a>
 
     </body>
 
--- admin_control_panel/orders.php
+++ admin_control_panel/orders.php
@@ -17,7 +17,7 @@ if ($_SESSION['auth_login'] == 'admin') { //выводим эту страниц
 
         case 'all-orders':
             $sort = "order_id DESC";
-            $sort_name = 'От А до Я';
+            $sort_name = 'без сортировки';
             break;
 
         case 'confirmed':
@@ -32,7 +32,7 @@ if ($_SESSION['auth_login'] == 'admin') { //выводим эту страниц
 
         default:
             $sort = "order_id DESC";
-            $sort_name = 'От А до Я';
+            $sort_name = 'без сортировки';
             break;
     }
 ?>
@@ -51,7 +51,7 @@ if ($_SESSION['auth_login'] == 'admin') { //выводим эту страниц
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
 
-        <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/orders/orders.css">
         <link rel="stylesheet" href="style/reviews/reviews.css">
         <link rel="stylesheet" href="jquery_confirm/jquery_confirm.css">
 
@@ -71,83 +71,85 @@ if ($_SESSION['auth_login'] == 'admin') { //выводим эту страниц
         $no_buy_count_result = mysqli_num_rows($no_buy_count);
 
         ?>
+        <div class="container block_cuntent">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="admin_control_panel.php">Главная</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Заказы</li>
+                </ol>
+            </nav>
 
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="admin_control_panel.php">Главная</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Заказы</li>
-            </ol>
-        </nav>
+            <?php include("panelyprav.php"); ?>
 
-        <?php include("panelyprav.php"); ?>
+            <div id="block-info">
+                <ul id="review-info-count">
+                    <li>Всего заказов - <strong>
+                            <? echo $all_count_result; ?></strong></li>
+                    <li>Обработаных - <strong>
+                            <? echo $buy_count_result; ?></strong></li>
+                    <li>Не обработаных - <strong>
+                            <? echo $no_buy_count_result; ?></strong></li>
+                </ul>
+            </div>
 
-
-        <div id="block-parameters">
-            <ul id="options-list">
-                <li>Сортировать:</li>
-                <li><a id="select-links" href="#">
-                        <? echo $sort_name; ?></a>
-                    <ul id="list-links-sort">
-                        <li><a href="orders.php?sort=all-orders">От А до Я</a></li>
-                        <li><a href="orders.php?sort=confirmed">Обработанные</a></li>
-                        <li><a href="orders.php?sort=no-confirmed">Не обработанные</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-
-
-        <div id="block-info">
-            <ul id="review-info-count">
-                <li>Всего заказов - <strong>
-                        <? echo $all_count_result; ?></strong></li>
-                <li>Обработаных - <strong>
-                        <? echo $buy_count_result; ?></strong></li>
-                <li>Не обработаных - <strong>
-                        <? echo $no_buy_count_result; ?></strong></li>
-            </ul>
-        </div>
+            <div id="block-parameters">
+                <ul id="options-list">
+                    <li>Сортировать: &nbsp;</li>
+                    <li><a id="select-links" href="#">
+                            <? echo $sort_name; ?></a>
+                        <ul id="list-links-sort">
+                            <li><a href="orders.php?sort=all-orders">без сортировки</a></li>
+                            <li><a href="orders.php?sort=confirmed">обработанные</a></li>
+                            <li><a href="orders.php?sort=no-confirmed">не обработанные</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
 
 
+            <div class="container-fluid">
 
-        <div class="container-fluid">
+                <?php
+                $result = mysqli_query($link, "SELECT * FROM orders ORDER BY $sort");
 
-            <?php
-            $result = mysqli_query($link, "SELECT * FROM orders ORDER BY $sort");
+                if (mysqli_num_rows($result) > 0) {
+                    $row = mysqli_fetch_array($result);
+                    do {
+                        $datetime = strtotime($row["order_datetime"]);
+                        if ($row["order_confirmed"] == 'yes') {
+                            $status = '<span class="green">Обработан</span>';
+                        } else {
+                            $status = '<span class="red">Не обработан</span>';
+                        }
 
-            if (mysqli_num_rows($result) > 0) {
-                $row = mysqli_fetch_array($result);
-                do {
-                    $datetime = strtotime($row["order_datetime"]);
-                    if ($row["order_confirmed"] == 'yes') {
-                        $status = '<span class="green">Обработан</span>';
-                    } else {
-                        $status = '<span class="red">Не обработан</span>';
-                    }
-
-                    echo '
+                        echo '
                     <div class="block-order">
-                    <p class="order-datetime" >' . date("d.m.Y h:i:s", $datetime)  . '</p>
-                    <p class="order-number" >Заказ № ' . $row["order_id"] . ' - ' . $status . '</p>
-                    <p class="order-link" ><a class="green" href="view_order.php?id=' . $row["order_id"] . '" >Подробней</a></p>
+                        <p class="order-datetime" >' . date("d.m.Y h:i:s", $datetime)  . '</p>
+                        <p class="order-number" >Заказ № ' . $row["order_id"] . ' - ' . $status . '</p>
+                        <p class="order-link" ><a class="green" href="view_order.php?id=' . $row["order_id"] . '" >Подробней</a></p>
                     </div>
                     ';
-                } while ($row = mysqli_fetch_array($result));
-            }
-            ?>
+                    } while ($row = mysqli_fetch_array($result));
+                }
+                ?>
 
 
 
+            </div>
+
+
+            <script defer type="text/javascript" src="../javascript/jquery-3.5.1.js"> </script>
+            <script defer type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+
+            <script defer type="text/javascript" src="../javascript/jquery-3.4.1.js"></script>
+            <script defer type="text/javascript" src="js/script.js"></script>
+            <script defer type="text/javascript" src="jquery_confirm/jquery_confirm.js"></script>
+            <script defer type="text/javascript" src="../javascript/header_footer.js"></script>
+
+            <script defer type="text/javascript" src="../javascript/scrollup.js"></script>
         </div>
-
-
-        <script defer type="text/javascript" src="../javascript/jquery-3.5.1.js"> </script>
-        <script defer type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
-
-        <script defer type="text/javascript" src="../javascript/jquery-3.4.1.js"></script>
-        <script defer type="text/javascript" src="js/script.js"></script>
-        <script defer type="text/javascript" src="jquery_confirm/jquery_confirm.js"></script>
-        <script defer type="text/javascript" src="../javascript/header_footer.js"></script>
+        <?php include("footer.php") ?>
+        <a href="#" class="scrollup">Наверх</a>
     </body>
 
     </html>
